b0f2fcd7bad17f2c7a88b7249c6a2f24346c656f
rafaaulbrich <rafaelacamilly2018@gmail.com>
2024-03-28 16:15:11 -0300

    Criação para Acessorio, Cor e Veiculo

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class Marca(models.Model):
     nacionalidade = models.CharField(max_length = 50, blank=True, null=True)
 
     def __str__(self):
-        return self.nome.toUpper()
+        return self.nome.upper()
 
 
 class Categoria(models.Model):
@@ -13,3 +13,30 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.descricao
+    
+
+class Cor(models.Model):
+    descricao = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.descricao
+    
+    class Meta:
+        verbose_name_plural = "Cores"
+    
+
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    cor = models.ForeignKey(Cor, on_delete=models.CASCADE)
+    ano = models.IntegerField(null=True, default = 0)
+
+    def __str__(self):
+        return f"{self.marca}, {self.categoria}, {self.ano}, {self.cor}"
