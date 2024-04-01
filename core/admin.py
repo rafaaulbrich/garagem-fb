24638e5371798768bff16ce0a5366dfbecda5af4
rafaaulbrich <rafaelacamilly2018@gmail.com>
2024-04-01 15:52:36 -0300

    Criação serializer Categoria

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import Marca, Categoria, Acessorio, Cor, Veiculo
-from .serializers import MarcaSerializer
+from .serializers import MarcaSerializer, CategoriaSerializer
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
