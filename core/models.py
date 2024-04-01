a6bb77a5f69915a318c67147c0468acd8590a5e0
rafaaulbrich <rafaelacamilly2018@gmail.com>
2024-04-01 15:37:15 -0300

    Alterações

--- core/models.py
+++ core/models.py
@@ -37,6 +37,7 @@ class Veiculo(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     cor = models.ForeignKey(Cor, on_delete=models.CASCADE)
     ano = models.IntegerField(null=True, default = 0)
+    preco = models.DecimalField(max_digits = 10, decimal_places = 2, null=True, default = 0)
 
     def __str__(self):
         return f"{self.marca}, {self.categoria}, {self.ano}, {self.cor}"
